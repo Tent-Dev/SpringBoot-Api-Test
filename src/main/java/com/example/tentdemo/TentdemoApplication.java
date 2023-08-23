856db4c8d2eca0f76a120a64b1e689b0192649c6
Chutipas Borsub <bo.chutipas@gmail.com>
2023-08-23 13:15:04 +0700

    First API test

---
+++ src/main/java/com/example/tentdemo/TentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentdemoApplication.class, args);
+	}
+
+}
