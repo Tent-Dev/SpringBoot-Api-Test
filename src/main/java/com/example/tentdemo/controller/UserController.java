d8cd82e43ffacefdf9fd696e17cafc39dac118d6
Chutipas Borsub <bo.chutipas@gmail.com>
2023-08-25 14:43:26 +0700

    Adding CRUD method

--- src/main/java/com/example/tentdemo/controller/UserController.java
+++ src/main/java/com/example/tentdemo/controller/UserController.java
@@ -3,11 +3,11 @@ package com.example.tentdemo.controller;
 import com.example.tentdemo.entity.User;
 import com.example.tentdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("users")
@@ -17,8 +17,32 @@ public class UserController {
     private UserService userService;
 
     // Get all users
-    @GetMapping
+    @GetMapping(value = "/getUserList")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    // Get user by ID
+    @GetMapping(value = "/getUserById/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    // Create a new user
+    @PostMapping(value = "/createNewUser", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    // Update user by ID
+    @PutMapping("/updateUserById/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return userService.updateUser(id, userDetails);
+    }
+
+    // Delete user by ID
+    @DeleteMapping("/deleteUserById/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
     }
 }
